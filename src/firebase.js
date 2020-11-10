79d01aae7a6cebdb29315c082e9f3a719d9d236f
Akshay Borana <akshayborana1@gmail.com>
2020-11-09 22:07:37 -0500

    feat: clearing files, and created header component.

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBse2sUjN8t3YQ2balPPrW95YToAcUCTo4",
+  authDomain: "facebook-clone-b319b.firebaseapp.com",
+  databaseURL: "https://facebook-clone-b319b.firebaseio.com",
+  projectId: "facebook-clone-b319b",
+  storageBucket: "facebook-clone-b319b.appspot.com",
+  messagingSenderId: "790579298663",
+  appId: "1:790579298663:web:dfc2abce65ef32842befc2",
+  measurementId: "G-96B0PP4H6W"
+};
