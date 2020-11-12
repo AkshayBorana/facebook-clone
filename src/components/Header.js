bc3a6420857a63510820347960fec28fc6ce9426
Akshay Borana <akshayborana1@gmail.com>
2020-11-11 21:18:04 -0500

    feat: building header component and added material UI

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,10 @@ import './Header.css'
 export const Header = () => {
     return (
         <div className="header">
-
+            <div className="header__left"></div>
+            {/* Add facebook logo here.... 31.00 */}
+            <div className="header__middle"></div>
+            <div className="header__right"></div>
         </div>
     )
 }
