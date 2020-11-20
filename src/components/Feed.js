241fc3fae51127f4c6b648f1e27e87fb16b06b1d
Akshay Borana <akshayborana1@gmail.com>
2020-11-19 19:50:32 -0500

    feat: feed component complete.

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './Feed.css';
+import { StoryReel } from './StoryReel';
 
 export const Feed = () => {
    return (
-        <div>
-
+        <div className="feed">
+            {/* Story Reel component */}
+            <StoryReel />
         </div>
    )
 }
---
+++ src/components/StoryReel.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import './StoryReel.css';
+import { Story } from './Story';
+
+export const StoryReel = () => {
+    return (
+        <div className="storyReel">
+            <Story
+                image=""
+                profileSrc="https://avatars3.githubusercontent.com/u/30196405?s=460&u=6bd3c8280b827a0ea3f661fc7c0c65117b19bc61&v=4"
+                title="Akshay"
+            />
+
+            <Story
+                image=""
+                profileSrc="https://avatars3.githubusercontent.com/u/30196405?s=460&u=6bd3c8280b827a0ea3f661fc7c0c65117b19bc61&v=4"
+                title="Sarah"
+            />
+
+            <Story
+                image=""
+                profileSrc="https://avatars3.githubusercontent.com/u/30196405?s=460&u=6bd3c8280b827a0ea3f661fc7c0c65117b19bc61&v=4"
+                title="Sean"
+            />
+
+            <Story
+                image=""
+                profileSrc="https://avatars3.githubusercontent.com/u/30196405?s=460&u=6bd3c8280b827a0ea3f661fc7c0c65117b19bc61&v=4"
+                title="Jenny"
+            />
+
+            <Story
+                image=""
+                profileSrc="https://avatars3.githubusercontent.com/u/30196405?s=460&u=6bd3c8280b827a0ea3f661fc7c0c65117b19bc61&v=4"
+                title="Tiffany"
+            />
+        </div>
+    )
+}
