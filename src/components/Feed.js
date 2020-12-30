5c662b95c423e4caef6e58e42d77b3cc5444f2c0
Akshay Borana <akshayborana1@gmail.com>
2020-12-30 16:29:14 -0500

    feat: building post component started.

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './Feed.css';
 import { StoryReel } from './StoryReel';
 import { MessageSender } from './MessageSender';
+import { Post } from './Post';
 
 export const Feed = () => {
    return (
@@ -9,6 +10,27 @@ export const Feed = () => {
             {/* Story Reel component */}
             <StoryReel />
             <MessageSender />
+            <Post
+                // username={username}
+                // message={message}
+                // timestamp={timestamp}
+                // profilePic={profilePic}
+                // image={image}
+            />
+            <Post
+                // username={username}
+                // message={message}
+                // timestamp={timestamp}
+                // profilePic={profilePic}
+                // image={image}
+            />
+            <Post
+                // username={username}
+                // message={message}
+                // timestamp={timestamp}
+                // profilePic={profilePic}
+                // image={image}
+            />
         </div>
    )
 }
